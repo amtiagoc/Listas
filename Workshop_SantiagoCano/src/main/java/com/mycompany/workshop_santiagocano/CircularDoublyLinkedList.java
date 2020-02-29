a5ee14cc448ff4372f2ffbb85400bfbbec3a2251
cano2030 <53246586+cano2030@users.noreply.github.com>
2020-02-29 12:12:48 -0500

    Add files via upload

--- Workshop_SantiagoCano/src/main/java/com/mycompany/workshop_santiagocano/CircularDoublyLinkedList.java
+++ Workshop_SantiagoCano/src/main/java/com/mycompany/workshop_santiagocano/CircularDoublyLinkedList.java
@@ -10,7 +10,7 @@ package com.mycompany.workshop_santiagocano;
  * @author scd
  * @param <T>
  */
-public class CircularDoublyLinkedList<T>  {
+public class CircularDoublyLinkedList<T extends Comparable> {
 
     DoubleNode<T> head;
 
@@ -20,7 +20,7 @@ public class CircularDoublyLinkedList<T>  {
 
     public void add(T d) {
         DoubleNode<T> newNode = new DoubleNode<>(d);
-        if (isEmpty()) {
+        if (IsEmpty()) {
             head = newNode;
             newNode.setNextNode(newNode);
             newNode.setPreviousNode(newNode);
@@ -36,7 +36,7 @@ public class CircularDoublyLinkedList<T>  {
 
     public void addLast(T d) {
         DoubleNode<T> newNode = new DoubleNode<>(d);
-        if (isEmpty()) {
+        if (IsEmpty()) {
             head = newNode;
             newNode.setNextNode(newNode);
             newNode.setPreviousNode(newNode);
@@ -45,18 +45,29 @@ public class CircularDoublyLinkedList<T>  {
             newNode.setNextNode(head);
             head.getPreviousNode().setNextNode(newNode);
             head.setPreviousNode(newNode);
-            //head=newNode;
-            //¿Se puede refactorizar?
-
         }
     }
 
     public void addOrdered(T d) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        DoubleNode<T> newNode = new DoubleNode<>(d);
+        if (IsEmpty() || d.compareTo(this.head.getData()) == 1) {
+            add(d);
+        } else {
+            DoubleNode<T> current = this.head;
+            while (d.compareTo(current.getNextNode().getData()) == -1 && current.getNextNode() != head) {
+                current = current.getNextNode();
+            }
+            if (current == head.getPreviousNode()) {
+                addLast(d);
+            } else {
+                newNode.setNextNode(current.getNextNode());
+                current.setNextNode(newNode);
+            }
+        }
     }
 
-    public void delete() throws Exception {
-        if (isEmpty()) {
+    public void Delete() throws Exception {
+        if (IsEmpty()) {
             throw new Exception("No existen datos por borrar...");
         } else if (head == head.getPreviousNode()) {
             head = null;
@@ -67,15 +78,11 @@ public class CircularDoublyLinkedList<T>  {
         }
     }
 
-    public void deleteLast() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public boolean isEmpty() {
+    public boolean IsEmpty() {
         return head == null;
     }
 
-    public String showData() {
+    public String ShowData() {
         String data = "";
         DoubleNode<T> current = this.head;
         do {
@@ -85,4 +92,52 @@ public class CircularDoublyLinkedList<T>  {
         return data;
     }
 
+    public boolean Exist(T name) {
+        if (IsEmpty()) {
+            return false;
+        } else {
+            DoubleNode<T> current = this.head;
+            do {
+                if (current.getData().compareTo(name) == 0) {
+                    return true;
+                }
+                current = current.getNextNode();
+            } while (current != head);
+            return false;
+        }
+    }
+
+    public DoubleNode<T> ItExist(T d) throws Exception {
+        if (IsEmpty()) {
+            throw new Exception("It is empty");
+        } else {
+            DoubleNode<T> current = this.head;
+            while (current.getNextNode() != head) {
+                if (current.getData().compareTo(d) == 0) {
+                    return current;
+                }
+                current = current.getNextNode();
+            }
+            return null;
+        }
+    }
+
+    public CircularDoublyLinkedList<T> DivideList(T d) {
+        try {
+            DoubleNode<T> current = ItExist(d);
+            if (current == null) {
+                System.out.println("No existe ese dato");
+            } else {
+                CircularDoublyLinkedList<T> newList = new CircularDoublyLinkedList<>();
+                while (current != head) {
+                    newList.addLast(current.getData());
+                    current = current.getNextNode();
+                }
+                return newList;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
 }
--- Workshop_SantiagoCano/src/main/java/com/mycompany/workshop_santiagocano/Operations.java
+++ Workshop_SantiagoCano/src/main/java/com/mycompany/workshop_santiagocano/Operations.java
@@ -9,7 +9,7 @@ package com.mycompany.workshop_santiagocano;
  *
  * @author cano2
  */
-public class Operations<T> {
+public class Operations<T extends Comparable> {
 
     DoubleSong<T> head;
 
@@ -36,7 +36,7 @@ public class Operations<T> {
         return head == null;
     }
     
-    public String showData(){
+    public String  ShowData(){
     String data = "";
          DoubleSong<T> current = this.head;
         do {
@@ -45,4 +45,104 @@ public class Operations<T> {
         } while (current != head);
         return data;
     }
+
+    public boolean Exist(T name){
+        if(IsEmpty()){
+            return false;
+        } else {
+            DoubleSong<T> current = this.head;
+            do{
+                if(current.getData().compareTo(name) == 0){
+                    return true;
+                }
+                current = current.getNextSong();
+            } while (current != head);
+            return false;
+        }
+    }
+         public String ShowPlayList() {
+        String data = "";
+        DoubleSong<T> current = this.head;
+        do {
+            data += current.getData()+ " \n ";
+            current = current.getNextSong();
+        } while (current != head);
+        return data;
+    }
+     
+         public void Delete() throws Exception {
+        if (IsEmpty()) {
+            throw new Exception("There are no data available to delete...");
+        } else if (head == head.getPreviousSong()) {
+            head = null;
+        } else {
+            head.getNextSong().setPreviousSong(head.getPreviousSong());
+            head.getPreviousSong().setNextSong(head.getNextSong());
+            head = head.getNextSong();
+        }
+    }
+         public void DeleteLast() throws Exception {
+        if(IsEmpty()){
+            throw new Exception("The list is empty...");
+        } else if (head==head.getPreviousSong()){
+            head = null;
+        } else {
+            DoubleSong<T> current = this.head;
+            while(current.getNextSong() != head.getPreviousSong()){
+                current = current.getNextSong();
+            }
+          current.setNextSong(head);
+            head.setPreviousSong(current);
+        }
+    }
+
+    public void DeleteSong(T name) throws Exception {
+        if (IsEmpty()) {
+            throw new Exception("No existen datos por borrar");
+        } else if (head == head.getPreviousSong()) {
+            head = null;
+        } else if (!Exist(name)){
+            throw new Exception("El dato no existe en la play list");
+        } else {
+            DoubleSong<T> current = this.head;
+            if(name.compareTo(head.getData()) == 0){
+                Delete();
+            } else if(name.compareTo(head.getPreviousSong().getData()) == 0){
+                DeleteLast();
+            }
+            do{
+                if(current.getNextSong().getData().compareTo(name) == 0){
+                    current.setNextSong(current.getNextSong().getNextSong());
+                }
+                current = current.getNextSong();
+            } while (current != head && current != head.getPreviousSong());
+        }
+    }
+    
+    public void update(T name, T newName) throws Exception{
+        if(IsEmpty()){
+            throw new Exception("There are no songs available to update, because is empty...");
+        } else if (!Exist(name)){
+            throw new Exception("That song does not exist...");
+        } else {
+            DoubleSong<T> current = this.head;
+            do {
+                if(current.getData().compareTo(name) == 0){
+                    current.setData(newName);
+                } 
+                current = current.getNextSong();
+            } while (current != head);
+        }
+    }
+        public String autoPlay(){
+        String data ="";
+        DoubleSong<T> current = this.head;
+        do{
+            data += current.getData()+ " ";
+            current = current.getNextSong();
+            
+        } while (current != head);
+        return data;
+    }
+         
 }
